5d6f405310f7171a40da71c532590ba52c9e30ff
priya-bhagat01 <pb337205@gmail.com>
2026-07-16 11:58:25 +0530

    Play game with keyboard (r, p, s)

--- RPSproject.js
+++ RPSproject.js
@@ -146,3 +146,13 @@ resetButton.addEventListener('click', () => {
     localStorage.removeItem('score');
     updateScore();
 })
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock')
+    } else if (event.key === 'p') {
+        playGame('paper')
+    } else if (event.key === 's') {
+        playGame('scissor')
+    }
+});
